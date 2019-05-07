5fcf38552b06711e67f038f2b446bde4435b2e97
Patrick <31744877+patricklee2@users.noreply.github.com>
2019-05-07 16:09:43 -0700

    Update init_container.sh

--- templates/main_images/init_container.sh
+++ templates/main_images/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-echo "Ruby version $RUBY_VERSION"
+Ruby version $RUBY_VERSION
 EOL
 cat /etc/motd
 
