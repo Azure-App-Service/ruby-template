8d0c8f574d778e32100905cec5c20ecf66d0a7ce
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-10-23 15:25:19 -0700

    banner

--- template/init_container.sh
+++ template/init_container.sh
@@ -11,6 +11,7 @@ A P P   S E R V I C E   O N   L I N U X
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
 Ruby version $RUBY_VERSION
+Note: Any data outside '/home' is not persisted
 EOL
 cat /etc/motd
 
